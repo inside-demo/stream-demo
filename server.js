e9c52fe4a1d3eb9771a9729c536c3042aef5807f
Ivan Demidov <scrum@list.ru>
2017-09-14 15:47:52 +0300

    tmp: commit

--- server.js
+++ server.js
@@ -1,36 +1,43 @@
-const app = require('express')();
-const server = require('http').createServer(app);
-const io = require('socket.io').listen(server);
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
-server.listen(8080);
+navigator.getUserMedia({
+    video: true,
+    audio: false
+  }, stream => {
+    const Peer = require('simple-peer');
+    const peer = new Peer({
+      initiator: location.hash === '#init',
+      trickle: false,
+      stream: stream
+    });
 
-app.get('/', function (req, res) { // При обращении к корневой странице
-  res.sendFile(__dirname + '/index.html'); // отдадим HTML-файл
-});
-io.sockets.on('connection', function (socket) {    // При подключении
-  socket.emit('server event', { hello: 'world' }); // отправим сообщение
-  socket.on('client event', function (data) {      // и объявим обработчик события при поступлении сообщения от клиента
-    console.log(data);
-  });
+    peer.on('signal', data => {
+      document.getElementById('yourId').value = JSON.stringify(data)
+    })
 
-  socket.on('offer', function (data) { // При получении сообщения 'offer',
-    // так как клиентское соединение в данном примере всего одно,
-    // отправим сообщение обратно через тот же сокет
-    // socket.emit('offer', data); 
-    // Если бы было необходимо переслать сообщение по всем соединениям, 
-    // кроме отправителя:
-    socket.broadcast.emit('offer', data);
-  });
-  socket.on('answer', function (data) {
-    socket.emit('answer', data);
-  });
-  socket.on('ice1', function (data) {
-    socket.emit('ice1', data);
-  });
-  socket.on('ice2', function (data) {
-    socket.emit('ice2', data);
-  });
-  socket.on('hangup', function (data) {
-    socket.emit('hangup', data);
-  });
-});
+    document.getElementById('connect').addEventListener('click', () => {
+      var otherId = JSON.parse(document.getElementById('otherId').value);
+
+      peer.signal(otherId);
+    });
+
+    document.getElementById('send').addEventListener('click', () => {
+      var yourMessage = document.getElementById('yourMessage').value;
+
+      peer.send(yourMessage);
+    });
+
+    peer.on('data', data => {
+      document.getElementById('message').textContent += `${data}\n`;
+    })
+
+    peer.on('stream', stream => {
+      var video = document.createElement('video');
+          video.srcObject = stream;
+
+      document.body.appendChild(video);
+      video.play();
+    })
+  }, err => {
+    console.log(err);
+  })
