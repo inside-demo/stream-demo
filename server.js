c660c65a53669f2a1ea56776afdacac5b4d7ba7f
Ivan Demidov <scrum@list.ru>
2017-09-19 14:22:51 +0300

    init

--- server.js
+++ server.js
@@ -1,36 +1,63 @@
-const app = require('express')();
-const server = require('http').createServer(app);
-const io = require('socket.io').listen(server);
+var os = require('os');
+// var nodeStatic = require('node-static');
+// var http = require('http');
+var socketIO = require('socket.io');
 
-server.listen(8080);
+// var fileServer = new(nodeStatic.Server)();
+// var app = http.createServer(function(req, res) {
+//   fileServer.serve(req, res);
+// }).listen(8080);
 
-app.get('/', function (req, res) { // При обращении к корневой странице
-  res.sendFile(__dirname + '/index.html'); // отдадим HTML-файл
-});
-io.sockets.on('connection', function (socket) {    // При подключении
-  socket.emit('server event', { hello: 'world' }); // отправим сообщение
-  socket.on('client event', function (data) {      // и объявим обработчик события при поступлении сообщения от клиента
-    console.log(data);
+var io = socketIO.listen(app);
+io.sockets.on('connection', function(socket) {
+
+  // convenience function to log server messages on the client
+  function log() {
+    var array = ['Message from server:'];
+    array.push.apply(array, arguments);
+    socket.emit('log', array);
+  }
+
+  socket.on('message', function(message) {
+    log('Client said: ', message);
+    // for a real app, would be room-only (not broadcast)
+    socket.broadcast.emit('message', message);
   });
 
-  socket.on('offer', function (data) { // При получении сообщения 'offer',
-    // так как клиентское соединение в данном примере всего одно,
-    // отправим сообщение обратно через тот же сокет
-    // socket.emit('offer', data); 
-    // Если бы было необходимо переслать сообщение по всем соединениям, 
-    // кроме отправителя:
-    socket.broadcast.emit('offer', data);
+  socket.on('create or join', function(room) {
+    log('Received request to create or join room ' + room);
+    var numClients = Object.keys(io.sockets.sockets).length;
+    log('Room ' + room + ' now has ' + numClients + ' client(s)');
+
+    if (numClients === 1) {
+      socket.join(room);
+      log('Client ID ' + socket.id + ' created room ' + room);
+      socket.emit('created', room, socket.id);
+    } else if (numClients === 2) {
+      log('Client ID ' + socket.id + ' joined room ' + room);
+      // io.sockets.in(room).emit('join', room);
+      socket.join(room);
+      socket.emit('joined', room, socket.id);
+      io.sockets.in(room).emit('ready', room);
+      socket.broadcast.emit('ready', room);
+    } else { // max two clients
+      socket.emit('full', room);
+    }
   });
-  socket.on('answer', function (data) {
-    socket.emit('answer', data);
+
+  socket.on('ipaddr', function() {
+    var ifaces = os.networkInterfaces();
+    for (var dev in ifaces) {
+      ifaces[dev].forEach(function(details) {
+        if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
+          socket.emit('ipaddr', details.address);
+        }
+      });
+    }
   });
-  socket.on('ice1', function (data) {
-    socket.emit('ice1', data);
+
+  socket.on('bye', function(){
+    console.log('received bye');
   });
-  socket.on('ice2', function (data) {
-    socket.emit('ice2', data);
-  });
-  socket.on('hangup', function (data) {
-    socket.emit('hangup', data);
-  });
+
 });
